60f9e48b1825506eb4f265e60063cdb678658eb0
Jared <jmtigers@udel.edu>
2024-10-11 12:15:52 -0400

    Formatted HashRoute; Created Home, BasicQuestions, DetailedQuestions

---
+++ src/pages/BasicQuestions.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+//import { Button, Form } from 'react-bootstrap';
+//import { HashRouter, Route } from 'react-router-dom';
+
+export function BasicQuestions():JSX.Element {
+    return <div></div>;
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+//import { Button, Form } from 'react-bootstrap';
+//import { HashRouter, Route } from 'react-router-dom';
+
+export function Home():JSX.Element {
+    return <div></div>;
+}
