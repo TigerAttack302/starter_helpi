48aae6d5ceda043909c5ed0097f27cc21594ea62
Jared <jmtigers@udel.edu>
2024-10-14 15:29:18 -0400

    Created two columns for basic and detailed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-//import { Button, Form } from 'react-bootstrap';
-//import { HashRouter, Route } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import './Home.css';
 
 export function Home():JSX.Element {
     const navigate = useNavigate();
     return (
         <div>
+            <div className="row">
+                <div className='column'>
             <h1>Basic Questions Page</h1>
-            <button onClick={() => navigate('/basic-questions')}>Go to Basic Questions</button>
+            <Button onClick={() => navigate('/basic-questions')}>Go to Basic Questions</Button>
             <p>This is the content of the Basic Questions page.</p>
-            <br />
+            </div>
+            <div className='column'>
             <h1>Detailed Questions Page</h1>
-            <button onClick={() => navigate('/detailed-questions')}>Go to Detailed Questions</button>
+            <Button onClick={() => navigate('/detailed-questions')}>Go to Detailed Questions</Button>
             <p>This is the content of the Detailed Questions page.</p>
+                </div>
+            </div>
         </div>
       );
 }
