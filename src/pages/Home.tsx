41da378f6808ef7ab008fa310a884c5e43ee6d45
Jared <jmtigers@udel.edu>
2024-10-14 14:55:31 -0400

    Added Buttons to navigate to basic and detailed pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 //import { Button, Form } from 'react-bootstrap';
 //import { HashRouter, Route } from 'react-router-dom';
 
 export function Home():JSX.Element {
-    return <div></div>;
+    const navigate = useNavigate();
+    return (
+        <div>
+            <h1>Basic Questions Page</h1>
+            <button onClick={() => navigate('/basic-questions')}>Go to Basic Questions</button>
+            <p>This is the content of the Basic Questions page.</p>
+            <br />
+            <h1>Detailed Questions Page</h1>
+            <button onClick={() => navigate('/detailed-questions')}>Go to Detailed Questions</button>
+            <p>This is the content of the Detailed Questions page.</p>
+        </div>
+      );
 }
