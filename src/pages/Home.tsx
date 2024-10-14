d2737f684d2662e4dc3c47d6629eee6c9c06eb75
Jared <jmtigers@udel.edu>
2024-10-14 15:49:51 -0400

    Modified homepage background and layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,17 +6,23 @@ import './Home.css';
 export function Home():JSX.Element {
     const navigate = useNavigate();
     return (
-        <div>
+        <div className='home'>
             <div className="row">
                 <div className='column'>
-            <h1>Basic Questions Page</h1>
-            <Button onClick={() => navigate('/basic-questions')}>Go to Basic Questions</Button>
-            <p>This is the content of the Basic Questions page.</p>
-            </div>
-            <div className='column'>
-            <h1>Detailed Questions Page</h1>
-            <Button onClick={() => navigate('/detailed-questions')}>Go to Detailed Questions</Button>
-            <p>This is the content of the Detailed Questions page.</p>
+                    <h1 className="header">Basic Questions</h1>
+                    <br />
+                    <div className="description">
+                        <p>This is the content of the Basic Questions page.</p>
+                        <Button onClick={() => navigate('/basic-questions')}>Go to Basic Questions</Button>
+                    </div>   
+                </div>
+                <div className='column'>
+                    <h1 className="header">Detailed Questions</h1>
+                    <br />
+                    <div  className="description">
+                        <p>This is the content of the Detailed Questions page.</p>
+                        <Button onClick={() => navigate('/detailed-questions')}>Go to Detailed Questions</Button>
+                    </div>
                 </div>
             </div>
         </div>
