2bdb187d44da45964a13b023dcd5ff9d300a87fb
Jared <jmtigers@udel.edu>
2024-10-25 12:52:10 -0400

    Removed unnecessary import

--- src/pages/BasicQuestions.tsx
+++ src/pages/BasicQuestions.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import './BasicQuestions.css';
 
