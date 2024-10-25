44d389cb3d5d72011175d4dc24bccbf642c72e58
Randolph Stokes <resavt@udel.edu>
2024-10-25 18:54:35 -0400

    changed the way that the questions and images are shown

--- src/pages/DetailedQuestions.tsx
+++ src/pages/DetailedQuestions.tsx
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Form } from 'react-bootstrap';
+import { Col, Container, Form, Row } from 'react-bootstrap';
+import f0f from './DetailedQuestionPictures/IMG.png';
+import full from './DetailedQuestionPictures/images.jpg';
+import principles from './DetailedQuestionPictures/download.jpg';
+import inspire from './DetailedQuestionPictures/inspirejpg.jpg';
+import strong from './DetailedQuestionPictures/stronk.jpg';
+import earth from './DetailedQuestionPictures/earth.jpg';
+import success from './DetailedQuestionPictures/images (1).jpg';
+import goal from './DetailedQuestionPictures/goal.png';
 import './DetailedQuestions.css';
 
 export function DetailedQuestions():JSX.Element {
@@ -46,9 +54,13 @@ export function DetailedQuestions():JSX.Element {
         <div className='ListOQues'>
           <h1 className='title'>Detailed Questions</h1>
         <Form.Group className="DetailedQues" controlId="DQlist">
+        <Container className='lowercontainer'>
+        <hr/>
+          <Row>
           <text>
           Q1. What activities or tasks make you feel the most energized and fulfilled, and why?
           </text>
+                <Col>
           <Form.Control
             className='textbox'
             value={ans1}
@@ -57,33 +69,67 @@ export function DetailedQuestions():JSX.Element {
             placeholder='Input Text'
             as="textarea"
             />
+            </Col>
+                <Col>
+                <img
+                  src={full}
+                  alt="img not found"
+                  className='IMG'
+                />
+                </Col> 
+              </Row>
+            <hr/>
             <text>
             Q2. What are your top three strengths or skills, and how do you apply them in your daily life?
             </text>
-          <Form.Control
-            className='textbox'
-            as="textarea"
-            value={ans2}
-            onChange={UpdateAns2}
-            role="textbox"
-            placeholder='Input Text'
-            />
-
+              <Row>
+                <Col>
+                  <Form.Control
+                    className='textbox'
+                    as="textarea"
+                    value={ans2}
+                    onChange={UpdateAns2}
+                    role="textbox"
+                    placeholder='Input Text'
+                  />
+                </Col>
+                <Col>
+                <img
+                  src={strong}
+                  alt="img not found"
+                  className='IMG'
+                />
+                </Col> 
+              </Row>
+          
+          <hr />
             <text>
             Q3. What are the values or principles that are most important to you in a work environment?
             </text>
+            <Row>
+                <Col>
             <Form.Control
               as="textarea"
               className='textbox'
               value={ans3}
-              
               onChange={UpdateAns3}
               placeholder='Input Text'
               />
-
+            </Col>
+                <Col>
+                <img
+                  src={principles}
+                  alt="img not found"
+                  className='IMG'
+                />
+                </Col> 
+            </Row>
+            <hr/>
             <text>
             Q4. What types of challenges or problems do you enjoy solving, and what makes them appealing?
             </text>
+            <Row>
+                <Col>
             <Form.Control
               className='textbox'
               value={ans4}
@@ -91,10 +137,21 @@ export function DetailedQuestions():JSX.Element {
               as="textarea"
               placeholder='Input Text'
               />
-
+              </Col>
+                <Col>
+                <img
+                  src={inspire}
+                  alt="img not found"
+                  className='IMG'
+                />
+                </Col> 
+            </Row>
+            <hr/>
             <text>
             Q5. What kind of impact do you want your work to have on others or the world?
             </text>
+            <Row>
+                <Col>
             <Form.Control
               className='textbox'
               value={ans5}
@@ -103,10 +160,21 @@ export function DetailedQuestions():JSX.Element {
               placeholder='Input Text'
               as="textarea"
               />
-
+              </Col>
+                <Col>
+                <img
+                  src={earth}
+                  alt="img not found"
+                  className='IMG'
+                />
+                </Col> 
+            </Row>
+            <hr/>
             <text>
             Q6. What are your financial goals and lifestyle preferences, and how do they influence your career choices?
             </text>
+            <Row>
+                <Col>
             <Form.Control
               className='textbox'
               value={ans6}
@@ -115,10 +183,21 @@ export function DetailedQuestions():JSX.Element {
               as="textarea"
               placeholder='Input Text'
               />
-
+              </Col>
+                <Col>
+                <img
+                  src={goal}
+                  alt="img not found"
+                  className='IMG'
+                />
+                </Col> 
+            </Row>
+            <hr/>
             <text>
             Q7. How do you define success in your career, and what will make you feel you’ve achieved it?
             </text>
+            <Row>
+                <Col>
             <Form.Control
               className='textbox'
               value={ans7}
@@ -127,6 +206,16 @@ export function DetailedQuestions():JSX.Element {
               as="textarea"
               placeholder='Input Text'
               />
+              </Col>
+                <Col>
+                <img
+                  src={success}
+                  alt="img not found"
+                  className='IMG'
+                />
+                </Col> 
+            </Row>
+          </Container>
       </Form.Group>
         </div>
         
