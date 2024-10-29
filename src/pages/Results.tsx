76ce9c5b95f7b56f4694a067ea2149b478fea8c8
Jared Murray <jmtigers@udel.edu>
2024-10-29 14:08:36 -0400

    Created navigation from results back to home

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-//import { useNavigate } from 'react-router-dom';
-
+import { useNavigate } from 'react-router-dom';
 import './Results.css';
 
 export function Results():JSX.Element {
+    const navigate = useNavigate();
+
     return (
-        <div></div>
+        <div>
+            <div className='home-button'>
+                <button onClick={() => navigate('/')}>Back to Home</button>
+            </div>
+            <hr/>
+        </div>
     )
 }
