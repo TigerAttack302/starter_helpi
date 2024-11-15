6c13fa300b1e52fb9f5478a434f770d8505d905c
TigerAttack302 <sonicm302@gmail.com>
2024-11-15 14:53:14 -0500

    Progress bar goes back down if you delete response - detailed

--- src/pages/DetailedQuestions.tsx
+++ src/pages/DetailedQuestions.tsx
@@ -27,66 +27,82 @@ export function DetailedQuestions():JSX.Element {
 
     const [ans1, setAns1] = useState<string>("");
     function UpdateAns1(event: React.ChangeEvent<HTMLInputElement>){
-        setAns1(event.target.value);
-        if (!ans1) {
-          setProgress(progress+14);
-        }
-        updateCompletion();
+      if (ans1 && !event.target.value) {
+        setProgress(progress-14);
+      } else if (!ans1 && event.target.value) {
+        setProgress(progress+14);
+      }
+      setAns1(event.target.value);
+      updateCompletion();
     }
 
     const [ans2, setAns2] = useState<string>("");
     function UpdateAns2(event: React.ChangeEvent<HTMLInputElement>){
-        setAns2(event.target.value);
-        if (!ans2) {
-          setProgress(progress+14);
-        }
-        updateCompletion();
+      if (ans2 && !event.target.value) {
+        setProgress(progress-14);
+      } else if (!ans2 && event.target.value) {
+        setProgress(progress+14);
+      }
+      setAns2(event.target.value);
+      updateCompletion();
     }
 
     const [ans3, setAns3] = useState<string>("");
     function UpdateAns3(event: React.ChangeEvent<HTMLInputElement>){
-        setAns3(event.target.value);
-        if (!ans3) {
-          setProgress(progress+14);
-        }
-        updateCompletion();
+      if (ans3 && !event.target.value) {
+        setProgress(progress-14);
+      } else if (!ans3 && event.target.value) {
+        setProgress(progress+14);
+      }
+      setAns3(event.target.value);
+      updateCompletion();
     }
     const [ans4, setAns4] = useState<string>("");
     function UpdateAns4(event: React.ChangeEvent<HTMLInputElement>){
-        setAns4(event.target.value);
-        if (!ans4) {
-          setProgress(progress+14);
-        }
-        updateCompletion();
+      if (ans4 && !event.target.value) {
+        setProgress(progress-14);
+      } else if (!ans4 && event.target.value) {
+        setProgress(progress+14);
+      }
+      setAns4(event.target.value);
+      updateCompletion();
     }
     const [ans5, setAns5] = useState<string>("");
     function UpdateAns5(event: React.ChangeEvent<HTMLInputElement>){
-        setAns5(event.target.value);
-        if (!ans5) {
-          setProgress(progress+14);
-        }
-        updateCompletion();
+      if (ans5 && !event.target.value) {
+        setProgress(progress-14);
+      } else if (!ans5 && event.target.value) {
+        setProgress(progress+14);
+      }
+      setAns5(event.target.value);
+      updateCompletion();
     }
     const [ans6, setAns6] = useState<string>("");
     function UpdateAns6(event: React.ChangeEvent<HTMLInputElement>){
-        setAns6(event.target.value);
-        if (!ans6) {
-          setProgress(progress+15);
-        }
-        updateCompletion();
+      if (ans6 && !event.target.value) {
+        setProgress(progress-15);
+      } else if (!ans6 && event.target.value) {
+        setProgress(progress+15);
+      }
+      setAns6(event.target.value);
+      updateCompletion();
     }
     const [ans7, setAns7] = useState<string>("");
     function UpdateAns7(event: React.ChangeEvent<HTMLInputElement>){
-        setAns7(event.target.value);
-        if (!ans7) {
-          setProgress(progress+15);
-        }
-        updateCompletion();
+      if (ans7 && !event.target.value) {
+        setProgress(progress-15);
+      } else if (!ans7 && event.target.value) {
+        setProgress(progress+15);
+      }
+      setAns7(event.target.value);
+      updateCompletion();
     }
       
     function updateCompletion() {
-      if (progress >= 85) {
-          setCompletion(true);
+      if (ans1 && ans2 && ans3 && ans4 && ans5 && ans6 && ans7) {
+        setCompletion(true);
+      } else if (!ans1 || !ans2 || !ans3 || !ans4 || !ans5 || !ans6 || !ans7) {
+        setCompletion(false);
       }
     }
 
