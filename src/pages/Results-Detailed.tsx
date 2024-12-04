c9844fc0284eccc46301bc7d1653a35a8d1aa056
TigerAttack302 <sonicm302@gmail.com>
2024-12-03 23:14:21 -0500

    Added two boxes: one telling the user what to do if ChatGPT isn't processing responses, the other giving the user suggestions on what to ask ChatGPT as follow-up

--- src/pages/Results-Detailed.tsx
+++ src/pages/Results-Detailed.tsx
@@ -65,17 +65,34 @@ export function ResultsDetailed():JSX.Element {
                 <button onClick={() => navigate('/')}>Back to Home</button>
                 <div className='results-header'><h1>Results</h1></div>
             </div>
-        <div className='CGPTresponse'>
-            <h3>ChatGPT response:</h3>
-            <div id="response"></div>
-        </div>
-        <div id="response1"></div>
-        <div className='communication'>
-            <Form.Control type="textarea" id="user-input" placeholder="Communicate with ChatGPT here..."/>
-            <Button onClick= {() => sendMessage((document.getElementById('user-input') as HTMLInputElement).value,'response1')} className='send-message' >Send</Button>
-        </div>
-        <script src="script.js" defer></script>
-        <hr/>
+            <div className='CGPTresponse'>
+                <h3>ChatGPT response:</h3>
+                <div id="response"></div>
+            </div>
+            <div id="response1"></div>
+            <div className='communication'>
+                <Form.Control type="textarea" id="user-input" placeholder="Communicate with ChatGPT here..."/>
+                <Button onClick= {() => sendMessage((document.getElementById('user-input') as HTMLInputElement).value,'response1')} className='send-message' >Send</Button>
+            </div>
+            <script src="script.js" defer></script>
+            <div className='results-row'>
+                <div className='suggestions'>
+                    <h1>Try asking some of these some questions!</h1>
+                    <br/>
+                    <ul>
+                        <li>Where can I find out more information about this career?</li>
+                        <li>What would be my first steps going into this career?</li>
+                        <li>How much does this career pay?</li>
+                        <li>I'm not sure if this career fits me, is there any similar career I might enjoy?</li>
+                    </ul>
+                </div>
+                <div className='error-handling'>
+                    <h1>If results are not processed...</h1>
+                    <br/>
+                    <p>If ChatGPT is not responding, and your results aren't showing, you may have to refresh the page and retake the assessment.
+                        Make sure your inserted API key is correct! Don't worry, this brew will be worth it!</p>
+                </div>
+            </div>
         </div>
     )
 }
